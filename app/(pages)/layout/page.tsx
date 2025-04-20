b8111ccdf3ffb0ef8d2dd4dd0fffaf8516ddf436
git-higaurav <dev.higaurav@gmail.com>
2025-04-21 00:34:56 +0100

    Feat:Landing Page Added

---
+++ app/(pages)/layout/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../header/page";
+import Landing from "../landing/page";
+
+export default function Layout(){
+    return(
+        <>
+            <Header/>
+            <Landing/>
+        </>
+    )
+}
