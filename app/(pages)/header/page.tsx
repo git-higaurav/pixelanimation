cca9dd1f163b3052d8a961359170806bc20d61f7
git-higaurav <dev.higaurav@gmail.com>
2025-04-20 15:30:40 +0100

    Feat: Header 2 updated

--- app/(pages)/header/page.tsx
+++ app/(pages)/header/page.tsx
@@ -1,9 +1,9 @@
-import { Clock, Facebook, Instagram, Mail, Phone, Youtube } from "lucide-react";
-
+import { AlignRight, Clock, Facebook, Instagram, Mail, Phone, Youtube } from "lucide-react";
+import Link from "next/link";
 export default function Header (){
     return(
-        <header className="flex justify-center items-center bg-title text-smoke">
-            <div className="wrapper w-full max-w-[1400px] py-3 lg:py-5 px-3">
+        <header className="flex justify-center items-center bg-title text-smoke flex-col">
+            <div className="wrapper w-full max-w-[1400px] py-2  px-3">
                 <div className="flex justify-center lg:justify-between">
                     <section className="flex gap-5">
                             <div className="flex items-center gap-5">
@@ -24,6 +24,24 @@ export default function Header (){
                         <Facebook className=" cursor-pointer"/>
                         <Instagram className=" cursor-pointer"/>
                         <Youtube className=" cursor-pointer"/>
+                    </section>
+                </div>
+            </div>
+            <div className="flex justify-center items-center w-full bg-smoke text-title py-5 px-3">
+                <div className="wrapper w-full max-w-[1400px] flex justify-between items-center">
+                    <section className="flex items-center gap-5">
+                        <img src="/logo/logo.png" alt="logo" className="max-w-[50px]" />
+                        <h1 className="text-title font-bold text-xl">Pixel Animation</h1>
+                        <div className="hidden md:flex md:items-center md:gap-5 ">
+                        <Link href={"#"}>Home</Link>
+                        <Link href={"#"}>Courses</Link>
+                        <Link href={"#"}>About Us</Link>
+                        <Link href={"#"}>Contact</Link>
+                        </div>
+                    </section>
+                    <section>
+                        <button className="bg-primary md:flex text-white py-3 px-8 hidden cursor-pointer ">Apply Now</button>
+                        <AlignRight className="md:hidden" size={40}  />
                     </section>
                 </div>
             </div>
